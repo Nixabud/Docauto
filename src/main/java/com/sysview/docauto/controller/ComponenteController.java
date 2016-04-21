d902b5e7816c8bd99fc40041aff20c3d98fe9324
Daniel Sanchez <jdaniel.saed@gmail.com>
2016-04-21 17:45:43 -0500

    se agrega RequestEntity como wrapper de response

--- src/main/java/com/sysview/docauto/controller/ComponenteController.java
+++ src/main/java/com/sysview/docauto/controller/ComponenteController.java
@@ -3,6 +3,10 @@ package com.sysview.docauto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,8 +26,9 @@ public class ComponenteController {
 		headers = "Accept=" + MediaType.APPLICATION_JSON_VALUE,
 		consumes = MediaType.APPLICATION_JSON_VALUE,
 		produces = MediaType.APPLICATION_JSON_VALUE)
-	 public List<Componente> componentesel(@RequestBody Componente componente){
-		return componenteService.filterByComponente(componente);		
+	 public ResponseEntity<List<Componente>> componentesel(@RequestBody Componente componente){
+		List<Componente> componentes = componenteService.filterByComponente(componente);
+		return new ResponseEntity<List<Componente>>(componentes, HttpStatus.OK);
 	}
 
 }
